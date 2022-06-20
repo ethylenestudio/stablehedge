2740df506e56909a5722f963983ec4a4c2fc20af
rersozlu <ersozlu.rafi@gmail.com>
2022-06-21 01:20:49 +0300

    init

--- contracts/StableHedge.sol
+++ contracts/StableHedge.sol
@@ -2,5 +2,67 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./interfaces/IJOERouter.sol";
 
-contract StableHedge {}
+//joe router contract: 0x60aE616a2155Ee3d9A68541Ba4544862310933d4
+//wavax address: 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7
+//usdc address: 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E
+
+error StableHedge__WrongPath(address requested, address correctAddress);
+
+contract StableHedge {
+    uint256 constant USDC_RATIO = 60; // dont forget to change
+    uint256 constant USDTE_RATIO = 40;
+    address constant USDC_ADDRESS = 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E; //will be given as constructor param in the prod
+    address constant WAVAX_ADDRESS = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7; //will be given as constructor param in the prod
+    IJOERouter immutable router;
+
+    mapping(address => Holding) public allHoldings;
+
+    struct Holding {
+        uint USDCHold;
+        uint USDTHold;
+    }
+
+    constructor() {
+        router = IJOERouter(0x60aE616a2155Ee3d9A68541Ba4544862310933d4); //will be given as constructor param in the prod
+    }
+
+    function deposit(
+        uint usdcOutMin,
+        address[] calldata USDCPath,
+        uint deadline
+    ) public payable returns (uint[] memory) {
+        require(msg.value > 0, "You can't deposit 0");
+        uint[] memory amounts = swapAvaxToStable(
+            usdcOutMin,
+            USDCPath,
+            address(this),
+            deadline
+        );
+        uint amountOfUSDC = amounts[amounts.length - 1];
+        uint newUSDCHold = allHoldings[msg.sender].USDCHold + amountOfUSDC;
+        allHoldings[msg.sender] = Holding(newUSDCHold, 0); //usdt is considered to be 0. reward and balance can be added to this struct
+        return amounts;
+    }
+
+    function swapAvaxToStable(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) private returns (uint[] memory) {
+        if (path[0] != WAVAX_ADDRESS) {
+            revert StableHedge__WrongPath(path[0], WAVAX_ADDRESS);
+        } else if (path[path.length - 1] != USDC_ADDRESS) {
+            revert StableHedge__WrongPath(path[path.length - 1], USDC_ADDRESS);
+        }
+        uint[] memory amounts = router.swapExactAVAXForTokens{value: msg.value}( //SENDING ALL VALUE ATM
+            amountOutMin,
+            path,
+            to,
+            deadline
+        );
+        return amounts;
+    }
+}
--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,43 @@
 const { ethers, waffle } = require("hardhat");
 const hre = require("hardhat");
+const USDC = require("./USDC.js");
 
 async function main() {
   //provider and signers
   const provider = waffle.provider;
   const [deployer, testAcc] = await hre.ethers.getSigners();
+  const usdcContract = await hre.ethers.getContractAt(
+    USDC,
+    "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E"
+  );
 
   //deploying contract
+
   const StableHedge = await hre.ethers.getContractFactory("StableHedge");
   const stableHedge = await StableHedge.deploy();
   await stableHedge.deployed();
 
   console.log("Contract deployed to:", stableHedge.address);
+  const amountIn = hre.ethers.utils.parseEther("1");
+  const swapAvaxToJoe = await stableHedge.deposit(
+    hre.ethers.utils.parseUnits("1", 6),
+    [
+      "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+      "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+    ],
+    "1655957322",
+    { value: hre.ethers.utils.parseEther("2") }
+  );
+  await swapAvaxToJoe.wait();
+
+  const newBalance = await usdcContract.balanceOf(stableHedge.address);
+  console.log(
+    `new usdc balance of the contract: ${hre.ethers.utils
+      .formatUnits(newBalance, 6)
+      .toString()}`
+  );
+  const holding = await stableHedge.allHoldings(deployer.address);
+  console.log(holding);
 }
 
 main()
